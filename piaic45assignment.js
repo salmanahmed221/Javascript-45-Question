23e8edcb1e57060f89c10c77fe8837a2566a5324
salmanahmed221 <115099021+salmanahmed221@users.noreply.github.com>
2022-11-23 20:51:20 +0500

    Javascript 45 question

---
+++ piaic45assignment.js
@@ -0,0 +1,542 @@
+// Question 1
+
+// var name1 = `"Hello Jessica, would you like to learn some python today?"`;
+// alert(name1);
+
+// Question 2
+
+// var name2 = "salMan";
+// alert(name2.toLowerCase());
+// alert(name2.toUpperCase());
+// alert(name2[0].toUpperCase() + name2.slice(1).toLowerCase());
+
+// Question 3
+
+// var quote = `Shakespeare's once said,"To be, or not to be: that is question"`;
+// alert(quote);
+
+// Question 4
+
+// var famous_person = `Shakespeare once said`;
+// var message1 = `"To be or not to be: that is question"`;
+// console.log(famous_person,message1);
+
+// Question 5
+
+// var name3 = "Salman\t Ahmed";
+// var name4 = 'Salman\n Ahmed';
+// var name5 = "Salman\"Ahmed";
+// alert(name3);
+// alert(name4);
+// alert(name5);
+
+// Question 6
+
+// var sum = 4 + 4;
+// alert(sum);
+// var sub = 12 - 4;
+// alert(sub);
+// var mul = 4 * 2;
+// alert(mul);
+// var div = 72 / 9;
+// alert(div);
+
+// Question 7
+
+// console.log(5 + 3);
+// console.log(12 - 4);
+// console.log(4 * 2);
+// console.log(72 / 9);
+
+// Question 8
+
+// var favorite_number = 9;
+// console.log("My favorite number is "+favorite_number);
+
+// Question 9
+
+// var favorite_number = 9;
+// console.log("My favorite number is "+favorite_number);
+// console.log(5 + 3);
+// console.log(12 - 4);
+// console.log(4 * 2);
+// console.log(72 / 9);
+
+// Question 10
+
+// var friendsname = ["Ali","Ahmed","Mubeen","Abdur Rehman"];
+// alert(friendsname[0]);
+// alert(friendsname[1]);
+// alert(friendsname[2]);
+// alert(friendsname[3]);
+
+// Question 11
+
+// var friendsname = ["Ali","Ahmed","Mubeen","Abdur Rehman"];
+// console.log("Hello",friendsname[0]);
+// console.log("Hello",friendsname[1]);
+// console.log("Hello",friendsname[2]);
+// console.log("Hello",friendsname[3]);
+
+// Question 12
+
+// var transportation = ["Honda","Toyota","Suzuki","Lancer","Bmw"];
+// console.log("I would like to own a",transportation[4]);
+
+// Question 13
+
+// var guest = ["Ahmed","Murtaza","Mubeen"];
+// console.log("Inviting them to dinner",guest[0]);
+// console.log("Inviting them to dinner",guest[1]);
+// console.log("Inviting them to dinner",guest[2]);
+
+// Question 14
+
+// var guest = ["Ahmed","Murtaza","Mubeen"];
+// console.log("Inviting them to dinner",guest[0]);
+// console.log("Inviting them to dinner",guest[1]);
+// console.log("Inviting them to dinner",guest[2]);
+// var guest = ["Ahmed","Murtaza","Mubeen"];
+// console.log("Not coming in dinner",guest[0]);
+// guest.splice(0,1,"Abdullah");
+// console.log("Inviting them to dinner",guest[0]);
+// console.log("Inviting them to dinner",guest[1]);
+// console.log("Inviting them to dinner",guest[2]);
+
+// Question 15
+
+// var guest = ["Ahmed","Murtaza","Mubeen"];
+// console.log("Inviting them to dinner",guest[0]);
+// console.log("Inviting them to dinner",guest[1]);
+// console.log("Inviting them to dinner",guest[2]);
+// var guest = ["Ahmed","Murtaza","Mubeen"];
+// console.log("Not coming in dinner",guest[0]);
+// guest.splice(0,1,"Abdullah");
+// console.log("Inviting them to dinner",guest[0]);
+// console.log("Inviting them to dinner",guest[1]);
+// console.log("Inviting them to dinner",guest[2]);
+// console.log("We got a bigger table");
+// guest.push("khubaib");
+// guest.push("faiz");
+// guest.push("Hasnain");
+// console.log("Inviting them to dinner",guest[0]);
+// console.log("Inviting them to dinner",guest[1]);
+// console.log("Inviting them to dinner",guest[2]);
+// console.log("Inviting them to dinner",guest[3]);
+// console.log("Inviting them to dinner",guest[4]);
+// console.log("Inviting them to dinner",guest[5]);
+
+// Question 16
+
+// var guest = ["Ahmed","Murtaza","Mubeen"];
+// console.log("Inviting them to dinner",guest[0]);
+// console.log("Inviting them to dinner",guest[1]);
+// console.log("Inviting them to dinner",guest[2]);
+// var guest = ["Ahmed","Murtaza","Mubeen"];
+// console.log("Not coming in dinner",guest[0]);
+// guest.splice(0,1,"Abdullah");
+// console.log("Inviting them to dinner",guest[0]);
+// console.log("Inviting them to dinner",guest[1]);
+// console.log("Inviting them to dinner",guest[2]);
+// console.log("We got a bigger table");
+// guest.push("khubaib");
+// guest.push("faiz");
+// guest.push("Hasnain");
+// console.log("Inviting them to dinner",guest[0]);
+// console.log("Inviting them to dinner",guest[1]);
+// console.log("Inviting them to dinner",guest[2]);
+// console.log("Inviting them to dinner",guest[3]);
+// console.log("Inviting them to dinner",guest[4]);
+// console.log("Inviting them to dinner",guest[5]);
+// console.log("We can only invite two people only");
+// guest.pop();
+// console.log("sorry not invited","Hasnain");
+// guest.pop();
+// console.log("sorry not invited","Faiz");
+// guest.pop();
+// console.log("sorry not invited","Khubaib");
+// guest.pop();
+// console.log("sorry not invited","Mubeen");
+// console.log("Inviting them to dinner",guest[0]);
+// console.log("Inviting them to dinner",guest[1]);
+// guest.pop();
+// guest.pop();
+// console.log(guest);
+
+// Question 17
+
+// var places = ["Naran","Kashmir","Murree","China border","pindipoint"];
+// console.log(places);
+// var places = ["Naran","Kashmir","Murree","China border","pindipoint"];
+// var alphabecticalorder = places.sort();
+// console.log(alphabecticalorder);
+// var places = ["Naran","Kashmir","Murree","China border","pindipoint"];
+// var reversealphabecticalorder = places.reverse();
+// console.log(reversealphabecticalorder);
+
+// Question 18
+
+// var guest = ["Ahmed", "Murtaza", "Mubeen"];
+// console.log("The number of people invited to dinner is", guest.length);
+
+// Question 19
+
+// var countries = ["England", "Portugal", "France", "Swizerland"];
+// for (var i = 0; i < countries.length; ++i) {
+//   console.log(countries[i]);
+// }
+
+// Question 20
+
+// var student = {
+//   Name: "ali",
+//   Age: "20",
+//   Class: "Piaic Web 3.o",
+//   Rollno: "23",
+// };
+// console.log(student);
+
+// Question 21
+
+// var food = ["Pizza", "Burger", "Fries", "Nuggets"];
+// console.log(food[4]);
+// food[4] = "Wings";
+// console.log(food);
+
+// Question 22
+
+// var car = "bmw";
+// console.log(car == "bmw");
+// var name = "salman";
+// console.log(name == "salman");
+// var age = 45;
+// console.log(age == 45);
+// var gender = "Male";
+// console.log(gender == "Male");
+// var sum = 2 + 2;
+// console.log(sum == 4);
+// var car = "bmw";
+// console.log(car == "Honda");
+// var name1 = "Salman";
+// console.log(name1 == "Ali");
+// var age = 56;
+// console.log(age == 54);
+// var gender1 = "Male";
+// console.log(gender1 == "female");
+// var sum = 2 + 2;
+// console.log(sum == 3);
+
+// Question 23
+
+// var fruit = "Mango";
+// console.log(fruit == "Mango");
+// var fruit1 = "Apple";
+// console.log(fruit1 == "apple");
+// var name = "Abid";
+// console.log(name == "Abid");
+// var age = 43;
+// console.log(age != 43);
+// var num = 6;
+// console.log(num < 9);
+// var num2 = 77;
+// console.log(num2 > 99);
+// var num3 = 44;
+// console.log(num3 <= 55);
+// var num4 = 77;
+// console.log(num4 <= 44);
+// var age = 18;
+// console.log(age > 17 && age < 25);
+// var age1 = 18;
+// console.log(age1 > 17 && age1 > 25);
+// var age2 = 45;
+// console.log(age2 > 40 || age2 < 50);
+// var age3 = 45;
+// console.log(age3 < 40 || age3 > 50);
+// var arr1 = ["pizza", "Burger", "Nuggets"];
+// console.log(arr1[2] == "Nuggets");
+// var arr2 = ["pizza", "Burger", "Nuggets"];
+// console.log(arr2[2] == "Burger");
+
+// Question 24
+
+// var alien_color = "green";
+// if (alien_color == "green") {
+//   console.log("The player just earned 5 points");
+// }
+// if (alien_color != "green") {
+//   console.log("no output");
+// }
+
+// Question 25
+
+// var alien_color = "red";
+// if (alien_color == "green") {
+//   console.log("The player just earned 5 points");
+// }
+// if (alien_color != "green") {
+//   console.log("The player just earned 10 ponits");
+// }
+// if (alien_color == "green") {
+//   console.log("The player just earned 5 points");
+// } else {
+//   console.log("Not matched");
+// }
+
+// Question 26
+
+// var alien_color = prompt("Enter the color?");
+// if (alien_color == "green") {
+//   console.log("The player earned 5 points");
+// }
+// if (alien_color == "yellow") {
+//   console.log("The player earned 10 points");
+// }
+// if (alien_color == "red") {
+//   console.log("The player earned 15 points");
+// }
+
+// Question 27
+
+// var age = prompt("Enter the age?");
+// if (age < 2) {
+//   console.log("The person is baby");
+// } else if (age >= 2 && age < 4) {
+//   console.log("The person  is toddler");
+// } else if (age >= 4 && age < 13) {
+//   console.log("The person is Kid");
+// } else if (age >= 13 && age < 20) {
+//   console.log("The person is teenager");
+// } else if (age >= 20 && age < 65) {
+//   console.log("The person is an adult");
+// } else if (age >= 65) {
+//   console.log("The person is an elder");
+// }
+
+// Question 28
+
+// var favorite_fruits = ["Apple", "Orange", "Peach"];
+// if (favorite_fruits.find((e) => e == "Apple")) {
+//   console.log("I really like apples!");
+// }
+
+// Question 29
+
+// var usernames = ["Admin", "Ali", "Ahmed", "Abdulah", "Adeel"];
+// for (var i = 0; i < usernames.length; ++i) {
+//   console.log("Hello", usernames[0], "would you like to see status reort? ");
+//   console.log("Hello", usernames[1], "thank you for logging in again");
+//   console.log("Hello", usernames[2], "thank you for logging in again");
+//   console.log("Hello", usernames[3], "thank you for logging in again");
+//   console.log("Hello", usernames[4], "thank you for logging in again ");
+//   break;
+// }
+
+// Question 30
+
+// var usernames = ["Admin", "Ali", "Ahmed", "Abdulah", "Adeel"];
+// if (usernames.length >= 0) {
+//   console.log("We need to find some user");
+// }
+
+// Question 31
+
+// var current_usernames = ["Admin", "Ali", "Ahmed", "Abdulah", "Adeel"];
+// var new_usernames = ["Ali", "Khubaib", "Salman", "Ahmed", "Shahid"];
+// for (var i = 0; i < new_usernames.length; ++i) {
+//   if (new_usernames[i] == "Ali") {
+//     console.log("The person need to enter a new username");
+//   } else if (new_usernames[i] == "Ahmed") {
+//     console.log("The person need to enter a new username");
+//   } else if (new_usernames[i] == "Khubaib") {
+//     console.log("The username is avaiable");
+//   } else if (new_usernames[i] == "Shahid") {
+//     console.log("The username is avaiable");
+//   } else if (new_usernames[i] == "Salman") {
+//     console.log("The username is avaiable");
+//   } else {
+//     console.log("Nothing Found");
+//   }
+// }
+
+// Question 32
+
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (var i = 0; i < numbers.length; ++i) {
+//   if (numbers[i] == 1) {
+//     console.log("1st");
+//   } else if (numbers[i] == 2) {
+//     console.log("2nd");
+//   } else if (numbers[i] == 3) {
+//     console.log("3rd");
+//   } else if (numbers[i] == 4) {
+//     console.log("4th");
+//   } else if (numbers[i] == 5) {
+//     console.log("5th");
+//   } else if (numbers[i] == 6) {
+//     console.log("6th");
+//   } else if (numbers[i] == 7) {
+//     console.log("7th");
+//   } else if (numbers[i] == 8) {
+//     console.log("8th");
+//   } else if (numbers[i] == 9) {
+//     console.log("9th");
+//   }
+// }
+
+// Question 33
+
+// var favorite_pizza = [
+//   "California Pizza",
+//   "New York-Style Pizza",
+//   "Greek Pizza",
+// ];
+// for (var i = 0; i < favorite_pizza.length; ++i) {
+//   if (favorite_pizza[i] == "California Pizza") {
+//     console.log("I like california pizza");
+//   } else if (favorite_pizza[i] == "New York-Style Pizza") {
+//     console.log("I like New York-Style Pizza");
+//   } else if (favorite_pizza[i] == "Greek Pizza") {
+//     console.log("I like Greek Pizza");
+//   }
+// }
+// console.log(
+//   "I like California pizza most because it is too much yummy and cheezy,such as i really love California pizza"
+// );
+
+// Question 34
+
+// var pets = ["Dogs", "Cats", "Goats"];
+// for (var i = 0; i < pets.length; ++i) {
+//   if (pets[i] == "Dogs") {
+//     console.log("A dog would make a great pet");
+//   } else if (pets[i] == "Cats") {
+//     console.log("A cat would make a great pet");
+//   } else if (pets[i] == "Goats") {
+//     console.log("A goat would make a great pet");
+//   }
+// }
+// console.log(
+//   "All of these animals have four legs but i think so,A dog would be a great pet"
+// );
+
+// Question 35
+
+// function make_shirt(size, message) {
+//   console.log("I am going to make", size, "tshirt");
+//   console.log("It will say", message);
+// }
+// make_shirt("Medium", "Gracias");
+
+// Question 36
+
+// function make_shirt(size, message) {
+//   console.log("The shirt are", size, "by default");
+//   console.log("It will say", message);
+// }
+// make_shirt("Large", "I love Javascript");
+
+// Question 37
+
+// function describe_city(city, country = "Pakistan") {
+//   console.log("The", city, "is in", country);
+// }
+// describe_city("Lahore");
+// describe_city("Karachi");
+// describe_city("Mumbai", "India");
+
+// Question 38
+
+// function city_country(city, country) {
+//   return city + "," + country;
+// }
+// var city = city_country("Lahore", "Pakistan");
+// console.log(city);
+// var city1 = city_country("Lisbon", "Portugal");
+// console.log(city1);
+// var city2 = city_country("Oslo", "NorWay");
+// console.log(city2);
+
+// Question 39
+
+// function make_album(artist, title) {
+//   album = {
+//     artist: artist,
+//     title: title,
+//   };
+//   return album;
+// }
+// var album1 = make_album("Closer", "The Chainsmoker");
+// console.log(album1);
+// var album2 = make_album("Lovely", "Billie Eilish");
+// console.log(album2);
+
+// Question 40
+
+// var magicains = ["David Copperfield", "Paul Danniel", "Teller"];
+// function show_magician(magicains) {
+//   console.log(magicains);
+// }
+// show_magician(magicains);
+
+// Question 41
+
+// var magicains = ["David Copperfield", "Paul Danniel", "Teller"];
+// function show_magician(magicains) {
+//   console.log(magicains);
+// }
+// show_magician(magicains);
+// function make_great(magicains) {
+//   console.log(magicains[0], "The great magicain!");
+//   console.log(magicains[1], "The great magicain!");
+//   console.log(magicains[2], "The great magicain!");
+// }
+// make_great(magicains);
+
+// Question 42
+
+// var magicains = ["David Copperfield", "Paul Danniel", "Teller"];
+// function show_magician(magicains) {
+//   console.log(magicains);
+// }
+// show_magician(magicains);
+// function make_great(magicains) {
+//   console.log("Great Magicain:");
+//   console.log(magicains[0], "The great magicain!");
+//   console.log(magicains[1], "The great magicain!");
+//   console.log(magicains[2], "The great magicain!");
+// }
+// make_great(magicains);
+// function original_magicain(magicains) {
+//   console.log("Original Magicain:");
+//   console.log(magicains);
+// }
+// original_magicain(magicains);
+
+// Question 43
+
+// var item1 = ["Roast beef", "Cheese", "Lettuce"];
+// var item2 = ["Ketchup", "Salad", "Honey mustard"];
+// var item3 = ["Peanut butter", "Stawbery jam", "Apple slices"];
+// function sandwich(item1) {
+//   console.log("I,ll make a good sandwich");
+//   console.log("Adding", item1[0], "to your sandwich");
+//   console.log("Adding", item1[1], "to your sandwich");
+//   console.log("Adding", item1[2], "to your sandwich");
+//   console.log("Your Sandwich is ready!");
+// }
+// sandwich(item1);
+// sandwich(item2);
+// sandwich(item3);
+
+// Question 44
+
+// function make_car(manufacturer, model, color) {
+//   car = {
+//     manufacturer: manufacturer,
+//     model: model,
+//     color: color,
+//   };
+//   return car;
+// }
+// var a = make_car("Honda", "City 2015", "White");
+// console.log(a);
